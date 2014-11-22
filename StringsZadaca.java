66b2a430c8078177c22dfcad8874f3e8f23bbc17
Vedad Zornic <vedadsa@windowslive.com>
2014-11-22 23:41:49 +0100

    Prepravka

--- StringsZadaca.java
+++ StringsZadaca.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 /**
- * Napišite program koji ucitava “string” i broj . Prvi parametar predstavlja neku
-
-reèenicu, a drugi parametar redni broj rijeèi unutar te reèenice. Program treba da 
-
-izdvoji tu rijeè iz reèenice, i da je ispise na ekran.
+ * Napišite program koji ucitava “string” i broj . Prvi parametar predstavlja
+ * neku
+ * 
+ * reèenicu, a drugi parametar redni broj rijeèi unutar te reèenice. Program
+ * treba da
+ * 
+ * izdvoji tu rijeè iz reèenice, i da je ispise na ekran.
+ * 
  * @author Vedad
  *
  */
@@ -25,31 +28,36 @@ public class StringsZadaca {
 		int j;
 		int brojac = 1;
 		int brojac1 = 1;
-        recenica = recenica.trim();
-        String razmak;
-       int brojRazmaka = 0;
-        
-        
-       
+		recenica = recenica.trim();
+		String noviPocetak;
+
 		// Trazenje rijeci
 		for (i = 0; i < (recenica.length() - 1); i++) {
 
 			if (recenica.charAt(i) == space) {
-				if  (recenica.charAt(i+1)==space  &&  recenica.charAt(i+1)==space){
-					
-							brojac=brojac-1;
+				if (recenica.charAt(i + 1) == space
+						&& recenica.charAt(i + 1) == space) {
+
+					brojac = brojac - 1;
 				}
-				brojac = brojac + 1;//Ukoliko je charAt(i) = 0, brojac povecavamo za 1.
+				brojac = brojac + 1;// Ukoliko je charAt(i) = 0, brojac
+									// povecavamo za 1.
 			}
 
 			if (brojac == broj) {
-				break;// Kada brojac bude jednak nasem unesenom broju, izlazimo iz petlje.
+				break;// Kada brojac bude jednak nasem unesenom broju, izlazimo
+						// iz petlje.
 			}
 
 		}
 
 		// odabir samo jedne rijeci // Pocinjemo od rijeci koja nam treba.
-		String noviPocetak = recenica.substring(i + 1);
+
+		if (broj == 1) {
+			noviPocetak = recenica.substring(i);
+		} else {
+			noviPocetak = recenica.substring(i + 1);
+		}
 		for (j = 0; j < (noviPocetak.length() - 1); j++) {
 
 			if (noviPocetak.charAt(j) == space) {
@@ -57,7 +65,8 @@ public class StringsZadaca {
 			}
 
 			if (noviPocetak.charAt(j) != space) {
-				brojac1 = brojac1 + 1;// Brojimo slova do razmaka da znamo velicinu rijeci.
+				brojac1 = brojac1 + 1;// Brojimo slova do razmaka da znamo
+										// velicinu rijeci.
 			}
 
 		}
@@ -65,6 +74,6 @@ public class StringsZadaca {
 
 		System.out.println("Rijec koju ste trazili je " + pravaRijec);
 		return;
-
 	}
+
 }
